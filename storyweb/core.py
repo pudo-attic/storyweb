f1fa5345dade98c05b1c86450f3da3f74a136c5e
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2014-11-13 11:43:57 +0100

    renamed to storyweb

--- storyweb/core.py
+++ storyweb/core.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask.ext.assets import Environment, Bundle
 from flask.ext.sqlalchemy import SQLAlchemy
 
-from narratives import default_settings
+from storyweb import default_settings
 
 logging.basicConfig(level=logging.DEBUG)
 
