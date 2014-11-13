3b1aa4b19098305d3d27eef39902b5ab78900972
Friedrich Lindenberg <friedrich@pudo.org>
2014-11-13 13:20:01 +0200

    Make the web application actually run.

--- storyweb/model.py
+++ storyweb/model.py
@@ -1,3 +1,22 @@
 from storyweb.core import db
+from storyweb.util import make_id
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    def __repr__(self):
+        return '<User(%r)>' % (self.id)
+
+
+class Block(db.Model):
+    id = db.Column(db.Unicode(40), primary_key=True, default=make_id)
+
+    def __repr__(self):
+        return '<Block(%r)>' % (self.id)
+
+
+
+
 
 
