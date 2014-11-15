a2cd95cf01ed0b258b915ac1366f6850103b6b0c
Friedrich Lindenberg <friedrich@pudo.org>
2014-11-15 21:17:07 +0200

    Admin for blocks.

--- storyweb/admin.py
+++ storyweb/admin.py
@@ -4,15 +4,19 @@ from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqla import ModelView
 
 from storyweb.core import app, db, app_name
-from storyweb.model import User
+from storyweb.model import User, Block
 
 
 class AppModelView(ModelView):
     
     def is_accessible(self):
-        if g.user is not None and g.user.is_active():
-            return True
-        return False
+        if g.user is None:
+            return False
+        if not g.user.is_active():
+            return False
+        if not g.user.is_admin:
+            return False
+        return True
 
     def _handle_view(self, name, **kwargs):
         if not self.is_accessible():
@@ -23,17 +27,24 @@ class UserAdmin(AppModelView):
     column_list = [
         'email',
         'display_name',
-        'active'
+        'active',
+        'is_admin',
+        'is_editor'
     ]
     column_exclude_list = ['password_hash']
 
     form_excluded_columns = [
         'password_hash',
+        'blocks',
+        'created_at',
+        'updated_at',
         'active'
     ]
 
     column_labels = {
         'email': 'E-Mail',
+        'is_admin': 'Administrator',
+        'is_editor': 'Editor',
         'display_name': 'Name'
     }
 
@@ -42,9 +53,33 @@ class UserAdmin(AppModelView):
         form_class.password = PasswordField('Password')
         return form_class
 
-    def on_model_change(self, form, model):
+    def on_model_change(self, form, model, is_created):
         model.password = form.password.data
+
+
+class BlockAdmin(AppModelView):
+    column_list = [
+        'text',
+        'source_label',
+        'date'
+    ]
+    
+    form_excluded_columns = [
+        'references',
+        'created_at',
+        'updated_at'
+    ]
+
+    column_labels = {
+        'text': 'Snippet',
+        'source_label': 'Source',
+        'source_url': 'Source link'
+    }
+
+    def on_model_change(self, form, model, is_created):
+        pass
 
 
 admin = Admin(app, name=app_name)
 admin.add_view(UserAdmin(User, db.session))
+admin.add_view(BlockAdmin(Block, db.session))
