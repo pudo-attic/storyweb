9ae9ce3abd2b03b3a06ccefc1d81727dac99eea2
Friedrich Lindenberg <friedrich@pudo.org>
2014-11-16 11:45:03 +0200

    location domain model type

---
+++ storyweb/model/location.py
@@ -0,0 +1,79 @@
+import logging
+from datetime import datetime
+from sqlalchemy import func
+from geopy import Nominatim
+
+from storyweb.core import db
+from storyweb.model.util import make_id, JSONEncodedDict
+from storyweb.model.user import User
+
+log = logging.getLogger(__name__)
+
+
+class Location(db.Model):
+    id = db.Column(db.Unicode(40), primary_key=True, default=make_id)
+    label = db.Column(db.Unicode)
+    is_geocoded = db.Column(db.Boolean, default=False)
+    country_code = db.Column(db.Unicode)
+    country = db.Column(db.Unicode)
+    state = db.Column(db.Unicode)
+    county = db.Column(db.Unicode)
+    city = db.Column(db.Unicode)
+    raw = db.Column(JSONEncodedDict)
+
+    author_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    author = db.relationship(User, backref=db.backref('locations',
+                             lazy='dynamic'))
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow,
+                           onupdate=datetime.utcnow)
+
+    def geocode(self):
+        if self.is_geocoded:
+            return
+        nom = Nominatim(timeout=6)
+        try:
+            loc = nom.geocode(self.label, exactly_one=True,
+                              addressdetails=True)
+            self.is_geocoded = True
+            if loc is None:
+                return
+            self.raw = loc.raw.get('address', {})
+            self.country_code = self.raw.get('country_code')
+            if self.country_code is not None:
+                self.country_code = self.country_code.upper()
+            self.country = self.raw.get('country')
+            self.state = self.raw.get('state') or \
+                self.raw.get('state_district')
+            self.state = self.raw.get('county')
+            self.city = self.raw.get('city') or \
+                self.raw.get('locality') or \
+                self.raw.get('village')
+        except Exception, e:
+            log.exception(e)
+
+    def __repr__(self):
+        return '<Location(%r)>' % (self.id)
+
+    @classmethod
+    def by_label(cls, label):
+        if label is None:
+            return None
+        q = db.session.query(cls)
+        q = q.filter(func.trim(func.lower(cls.label)) ==
+                     label.lower().strip())
+        return q.first()
+
+    @classmethod
+    def lookup(cls, label, author):
+        log.info('Looking up: %s', label)
+        location = cls.by_label(label)
+        if location is None:
+            location = cls()
+            location.label = label
+            location.author = author
+            location.geocode()
+            db.session.add(location)
+        return location
+
--- storyweb/model/util.py
+++ storyweb/model/util.py
@@ -1,5 +1,9 @@
 import string
 from uuid import uuid4
+import json
+
+from sqlalchemy.types import TypeDecorator, VARCHAR
+from sqlalchemy.ext.mutable import Mutable
 
 ALPHABET = string.ascii_uppercase + string.ascii_lowercase + string.digits
 
@@ -14,3 +18,45 @@ def make_id():
             break
     return ''.join(reversed(s))
 
+
+class JSONEncodedDict(TypeDecorator):
+    "Represents an immutable structure as a json-encoded string."
+
+    impl = VARCHAR
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
+
+
+class MutableDict(Mutable, dict):
+    @classmethod
+    def coerce(cls, key, value):
+        "Convert plain dictionaries to MutableDict."
+
+        if not isinstance(value, MutableDict):
+            if isinstance(value, dict):
+                return MutableDict(value)
+
+            # this call will raise ValueError
+            return Mutable.coerce(key, value)
+        else:
+            return value
+
+    def __setitem__(self, key, value):
+        "Detect dictionary set events and emit change events."
+
+        dict.__setitem__(self, key, value)
+        self.changed()
+
+    def __delitem__(self, key):
+        "Detect dictionary del events and emit change events."
+
+        dict.__delitem__(self, key)
+        self.changed()
