f22591c065d2e49936f3f70e3640fdeedf0d0d01
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-11 17:24:19 +0200

    Begin working on search, just the ability to pick suggested entities for now (refs #8).

--- storyweb/static/js/controllers/app.js
+++ storyweb/static/js/controllers/app.js
@@ -1,7 +1,26 @@
 storyweb.controller('AppCtrl', ['$scope', '$location', '$http', 'cfpLoadingBar',
   function($scope, $location, $http, cfpLoadingBar) {
 
+  $scope.searchQuery = '';
   $scope.session = {'logged_in': false, 'is_admin': false};
+  
+
+  $scope.search = function() {
+    console.log("TODO Search: " + $scope.searchQuery);
+  }
+
+  $scope.searchGo = function($item) {
+    $scope.searchQuery = '';
+    $location.path('/cards/' + $item.id);
+  }
+
+  $scope.searchSuggest = function(q) {
+    var params = {'prefix': q};
+    return $http.get('/api/1/cards/_suggest', {'params': params}).then(function(res) {
+      return res.data.options;
+    });
+  }
+
   $http.get('/api/1/session').then(function(res) {
     $scope.session = res.data;
     if (!$scope.session.logged_in) {
