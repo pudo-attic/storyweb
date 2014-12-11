4e0103fff54970aa7b53b8542eb28d537db90601
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-11 23:56:15 +0200

    Minor refactorings, and search controller, refs #8.

--- storyweb/static/js/controllers/article_list.js
+++ storyweb/static/js/controllers/article_list.js
@@ -1,12 +1,12 @@
 storyweb.controller('ArticleListCtrl', ['$scope', '$location', '$http', 'cfpLoadingBar',
   function($scope, $location, $http, cfpLoadingBar) {
 
-  $scope.stories = [];
+  $scope.articles = [];
 
   cfpLoadingBar.start();
   var params = {'category': 'Article'};
   $http.get('/api/1/cards', {params: params}).then(function(res) {
-    $scope.stories = res.data.results;
+    $scope.articles = res.data.results;
     cfpLoadingBar.complete();
   });
 
---
+++ storyweb/static/js/controllers/search.js
@@ -0,0 +1,7 @@
+storyweb.controller('SearchCtrl', ['$scope', '$location', '$http', 'cfpLoadingBar',
+  function($scope, $location, $http, cfpLoadingBar) {
+
+  console.log('Query', $scope.searchQuery);
+
+}]);
+
