75cc48344283f0168afa5fd1bd9efff3d601a66d
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-11 15:31:00 +0200

    project files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='tmi',
+    name='storyweb',
     version='0.1',
     description="Create story networks out of text snippets.",
     long_description="",
