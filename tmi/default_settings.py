1e3efdc30d60b8e2c16380a06e138f4b3efda881
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-11 07:37:15 +0200

    Rename env vars, fixes #11.

--- tmi/default_settings.py
+++ tmi/default_settings.py
@@ -1,26 +1,24 @@
-import os
+from os import environ as env
 
 APP_NAME = 'tmi'
-PROJECT_NAME = os.environ.get('PROJECT_NAME', 'Grano StoryWeb')
-PROJECT_DESCRIPTION = os.environ.get('PROJECT_DESCRIPTION',
-                                     'You know, an IDE for news!')
+APP_TITLE = env.get('APP_TITLE', 'Grano StoryWeb')
+APP_DESCRIPTION = env.get('APP_DESCRIPTION', 'You know, an IDE for news!')
 
-DEBUG = os.environ.get('DEBUG', '').lower().strip()
+DEBUG = env.get('DEBUG', '').lower().strip()
 DEBUG = DEBUG in ['no', 'false', '0']
 ASSETS_DEBUG = DEBUG
-SECRET_KEY = os.environ.get('SECRET_KEY', 'banana pancakes')
+SECRET_KEY = env.get('SECRET_KEY', 'banana pancakes')
 
 db_uri = 'sqlite:///%s.sqlite3' % APP_NAME
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', db_uri)
-ELASTICSEARCH_URL = os.environ.get('BONSAI_URL',
-                                   'http://localhost:9200')
+SQLALCHEMY_DATABASE_URI = env.get('DATABASE_URL', db_uri)
+ELASTICSEARCH_URL = env.get('BONSAI_URL', 'http://localhost:9200')
 
-OPENCORPORATES_KEY = os.environ.get('OPENCORPORATES_KEY')
-CALAIS_KEY = os.environ.get('CALAIS_KEY')
+OPENCORPORATES_KEY = env.get('OPENCORPORATES_KEY')
+CALAIS_KEY = env.get('CALAIS_KEY')
 
 CELERY_ALWAYS_EAGER = True
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TIMEZONE = 'UTC'
-CELERY_BROKER_URL = os.environ.get('RABBITMQ_BIGWIG_URL',
-                                   'amqp://guest:guest@localhost:5672//')
+CELERY_BROKER_URL = env.get('RABBITMQ_BIGWIG_URL',
+                            'amqp://guest:guest@localhost:5672//')
