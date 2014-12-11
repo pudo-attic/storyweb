0a274419b841dd5410da9bcab7e7e61adad4e532
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-11 23:09:00 +0200

    Lookup while adding links to a card.

--- storyweb/static/js/directives/new_link.js
+++ storyweb/static/js/directives/new_link.js
@@ -18,29 +18,42 @@ storyweb.directive('storywebNewLink', ['$http', 'cfpLoadingBar', function($http,
         return scope.card.title && scope.card.title.length > 1;
       };
 
+      scope.suggestCard = function(q) {
+        console.log(q);
+        var params = {'prefix': q};
+        return $http.get('/api/1/cards/_suggest', {'params': params}).then(function(res) {
+          return res.data.options;
+        });
+      };
+
+      scope.linkCard = function(card) {
+        saveLink(card);
+      };
+
       scope.saveCard = function() {
         if (!scope.canSubmit()) return;
         cfpLoadingBar.start();
         var card = angular.copy(scope.card);
         // create new card
-        scope.card = {'category': 'Company'};
         $http.post('/api/1/cards', card).then(function(res) {
-          var child = {
-            'child': res.data.id,
-            'status': 'approved',
-            'offset': 0
-          };
-          // link to story
-          var url = '/api/1/cards/' + scope.parent.id + '/links';
-          scope.$emit('pendingTab');
-          $http.post(url, child).then(function(res) {
-            scope.card = res.data;
-            cfpLoadingBar.complete();
-          });
+          saveLink(res.data);
         });
 
+        scope.card = {'category': 'Company'};
       };
 
+      var saveLink = function(child) {
+        var link = {
+          'child': child,
+          'status': 'approved',
+          'offset': 0
+        };
+        var url = '/api/1/cards/' + scope.parent.id + '/links';
+        $http.post(url, link).then(function(res) {
+          cfpLoadingBar.complete();
+        });
+        scope.$emit('pendingTab');
+      };
     }
   };
 }]);
