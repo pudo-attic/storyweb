1cb2dbedbafec78a59c3faaf5ee0801cbd0a5911
Friedrich Lindenberg <friedrich@pudo.org>
2014-12-12 20:20:14 +0200

    Query filtering (finally), refs #8.

--- storyweb/search/queries.py
+++ storyweb/search/queries.py
@@ -1,4 +1,21 @@
 
 
 def cards_query(req):
-    return {}
+    qstr = req.get('q', '').strip()
+    if len(qstr):
+        q = {'query_string': {'query': qstr}}
+        bq = [
+            {"term": {"title": {"value": qstr, "boost": 10.0}}},
+            {"term": {"aliases": {"value": qstr, "boost": 6.0}}},
+            {"term": {"text": {"value": qstr, "boost": 3.0}}}
+        ]
+        q = {
+            "bool": {
+                "must": q,
+                "should": bq
+            }
+        }
+        print q
+    else:
+        q = {'match_all': {}}
+    return {'query': q}
